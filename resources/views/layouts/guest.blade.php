72858174fb1a101ce17279b710c2e98abae70090
MaccCode <abadmarcneil16@gmail.com>
2026-07-21 22:55:56 +0800

    another 1

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" href="ID/logo.png" type="image/png">
+        <link rel="icon" href="{{ asset('ID/logo.png') }}" type="image/png">
 
         <title>Escapedia</title>
 
